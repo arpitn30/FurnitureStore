abcf21cf8bbdc3500e786b6700277f84fa967cf0
Arpit <arpit.nandwani@gmail.com>
2019-04-02 16:47:17 +0530

    Updated SQL Statements

--- shop.sql
+++ shop.sql
@@ -6,9 +6,14 @@ CREATE  TABLE `shop`.`users` (
   `email` VARCHAR(45) NOT NULL ,
   `password` VARCHAR(45) NOT NULL ,
   `balance` BIGINT ZEROFILL UNSIGNED NULL ,
-  `security` VARCHAR(100) NULL ,
+  `secQues` VARCHAR(100) NULL ,
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) ,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) )
 COMMENT = 'The tables to store the details of the users of the Online F' /* comment truncated */;
+
+
+INSERT INTO `shop`.`users` (`name`, `email`, `password`, `balance`, `secQues`) VALUES ('Arpit', 'arpit@altran.com', '456123', 10000, 'gagan');
+
+
 
