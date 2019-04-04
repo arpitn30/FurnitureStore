5debd28ab070d4ab55055627ed5a14a7103eb542
Arpit <arpit.nandwani@gmail.com>
2019-04-04 17:18:23 +0530

    Added admin and user class

---
+++ src/main/java/com/shop/furniture/User.java
@@ -0,0 +1,37 @@
+/** *********************************************************************
+*                        Aricent ALTRAN group Proprietary
+*
+*	This source code is the sole property of Aricent ALTRAN group. Any form
+*	of utilization of this source code in whole or in part is  prohibited
+*	without  written consent from Aricent ALTRAN group.
+*
+*	  File Name	            	:	User.java
+*	  Principal Author      	:	Arpit Nandwani
+*	  Subsystem Name        	:
+*	  Module Name           	:	com.shop.furniture
+*	  Date of First Release 	:	
+*	  Author					:	
+*	  Description           	:
+*
+*
+*	  Change History
+*
+*	  Version      				:	1.0
+*	  Date(DD/MM/YYYY) 			:	Apr 4, 2019
+*	  Modified by				:	Arpit Nandwani
+*	  Description of change 	:
+*
+	**********************************************************************
+*/
+package com.shop.furniture;
+
+import javax.ws.rs.Path;
+
+/**
+ * @author Arpit Nandwani
+ *
+ */
+@Path("/user")
+public class User {
+
+}
